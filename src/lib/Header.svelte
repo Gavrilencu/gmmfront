2a0c5aa2500b1690a8ce4a5f5277e75e7fe364e6
Your Name <you@example.com>
2024-04-18 21:37:46 +0300

    v555

--- src/lib/Header.svelte
+++ src/lib/Header.svelte
@@ -23,25 +23,15 @@
   <div class="carousel-inner">
     <div class="carousel-item active" data-bs-interval="2000">
       <img src="{image1}" class="d-block w-100 imageconfig" alt="...">
-      <div class="carousel-caption d-none d-md-block">
-        <h5 class="white">Teste PCR</h5>
-        <p class="white">Testele PCR, care detectează secvențe specifice de ADN sau ARN, sunt fundamentale în diagnosticarea bolilor, cercetarea genetică și monitorizarea sănătății publice.</p>
-      </div>
+     
     </div>
     <div class="carousel-item" data-bs-interval="2000">
       <img src="{image2}" class="d-block w-100 imageconfig" alt="...">
-      <div class="carousel-caption d-none d-md-block">
-        <h5 class="white">ADN</h5>
-        <p class="white">
-          ADN-ul (acidul dezoxiribonucleic) este materialul genetic esențial care ghidează dezvoltarea și funcționarea tuturor organismelor vii.</p>
-      </div>
+     
     </div>
     <div class="carousel-item" data-bs-interval="2000">
       <img src="{image3}" class="d-block w-100 imageconfig" alt="...">
-      <div class="carousel-caption d-none d-md-block">
-        <h5 class="white">Medii Nutriente</h5>
-        <p class="white">Nutrienții importați sunt esențiali pentru cultivarea diverselor tipuri de celule și microorganisme în cercetările biologice și medicale.</p>
-      </div>
+     
     </div>
   </div>
 
